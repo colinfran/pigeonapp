5f14fb436ed18618f4089c47b97ee9fc9fc20e29
Jack Newman <jack.newman96@gmail.com>
2018-11-10 18:55:05 -0800

    First step in firebase integration

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
 
+import * as firebase from 'firebase';
+
 import { getRootNavigator } from './views'
 import { isLoggedIn } from './api/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDiFNSIiZO0EUh0jMiYwiDSYJ2wRRl7j4s',
+  authDomain: 'project-pigeon-28cbf.firebaseapp.com',
+  databaseURL: 'https://project-pigeon-28cbf.firebaseio.com',
+  storageBucket: 'project-pigeon-28cbf.appspot.com'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref('/test_users/RNl4bSjqLLjTm1BWwX9l').set({
+  'phone_number': '123-123-1234'
+});
+
+
 
 export default class Root extends Component {
   constructor(props) {
